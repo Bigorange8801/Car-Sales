f2c6428b3cccc5240a72914f3ad9969d8733d6b8
John <cjd8801@gmail.com>
2020-03-18 18:05:03 -0500

    mvp

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
-
+// import { buyFeature } from '../actions/index';
+// import { connect } from 'react-redux';
 const AdditionalFeatures = props => {
   return (
     <div className="content">
@@ -8,7 +9,7 @@ const AdditionalFeatures = props => {
       {props.store.length ? (
         <ol type="1">
           {props.store.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} buyFeature={props.buyFeature}feature={item} />
           ))}
         </ol>
       ) : (
@@ -17,5 +18,6 @@ const AdditionalFeatures = props => {
     </div>
   );
 };
+
 
 export default AdditionalFeatures;
